af8dc689eed4a21467dfa546775dd93d03e5e021
Alex Maximiano <alexgamadev@gmail.com>
2024-05-21 08:41:44 -0300

    update: change urls

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("finance/", include("finance.urls")),
+    path("", include("finance.urls")),
 ]
